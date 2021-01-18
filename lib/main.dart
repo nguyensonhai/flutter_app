48363721f050dd16f3babdc00f2701c811cf1076
nguyensonhai <nguyensonhai1009@gmail.com>
2021-01-18 17:28:49 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -8,27 +8,30 @@ void main() {
 
 class Home extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("My First Flutter App"),
         centerTitle: true,
         backgroundColor: Colors.greenAccent,
       ),
-      body:  Container(
-         child: Column(
-           children: [
-             Text("Hello, this is Nero!!!",
-                 style: TextStyle(
-                     fontSize: 30.0,
-                     fontFamily: "IndieFlower"
-                 )
-             ),
-             Image.asset("images/nero.png")
-           ],
-           // mainAxisAlignment: MainAxisAlignment.center,
-           // crossAxisAlignment: CrossAxisAlignment.center,
-         ),
+      body: Container(
+        child: Column(
+          children: [
+            Text("Hello, this is Nero!!!",
+                style: TextStyle(fontSize: 30.0, fontFamily: "IndieFlower")),
+            Image.asset("images/nero.png"),
+            RaisedButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.mail),
+              label: Text("Mail me"),
+              textColor: Colors.black,
+              color: Colors.greenAccent,
+            )
+          ],
+          // mainAxisAlignment: MainAxisAlignment.center,
+          // crossAxisAlignment: CrossAxisAlignment.center,
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
